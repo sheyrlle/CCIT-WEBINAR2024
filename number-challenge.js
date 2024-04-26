cf05ac03a943e0153dcb41a619874b08cc156e81
sheyrlle <168162033+sheyrlle@users.noreply.github.com>
2024-04-26 14:02:04 +0800

    Add files via upload

---
+++ number-challenge.js
@@ -0,0 +1,25 @@
+function sumthree(num1, num2, num3) {
+    const sum = num1 + num2 + num3;
+    return sum;
+  }
+  
+  function differencetwo(num1, num2) {
+    const difference = num1 - num2;
+    return difference;
+  }
+  
+  function producttwo(num1, num2) {
+    const product = num1 * num2;
+    return product;
+  }
+  
+  function averagetwo(num1, num2) {
+    const average = (num1 + num2) / 2;
+    return average;
+  }
+  
+  console.log(sumthree(5, 3, 2));
+  console.log(differencetwo(10, 4));
+  console.log(producttwo(6, 7));
+  console.log((8, 9));
+  
